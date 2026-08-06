b9fdbed505eb81671b9e3bd8b1d69d11ddf15227
Oliber1411 <Oliver.vanraalte@education.nsw.gov.au>
2026-08-06 10:41:20 +1000

    Added my to do list
    It begins.

---
+++ main.py
@@ -0,0 +1,6 @@
+#TO DO:
+#Create function that detects the current volume
+#Create function that detects if an object infront of it has moved
+#Create a function that plays a buzzer
+#Put 'em allll together
+
